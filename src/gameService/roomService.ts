9a692d878b7f31cbeaa361856022e68aa7125729
MOONcitizenX <mooncitizenx@gmail.com>
2023-02-16 22:23:40 +0300

    fix: fix startgame logic

--- src/gameService/roomService.ts
+++ src/gameService/roomService.ts
@@ -66,12 +66,8 @@ export class Room {
       const cards = Object.keys(cardsMap);
       const shuffledCards = shuffle(cards);
       this.closedDeck = shuffledCards;
-      this.players.forEach((player, index) => {
-        if (index === 0) {
-          player.cards = ['8R', '8B'];
-        } else {
-          player.cards = this.closedDeck.splice(-5, 5);
-        }
+      this.players.forEach((player) => {
+        player.cards = this.closedDeck.splice(-5, 5);
       });
       const startCard = this.closedDeck.pop();
       if (startCard) {
